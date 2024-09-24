0b8b3a5c80e82fa5fedd4ad817cc290c4167f8d4
Dileep <ddr.dileepraloti@gmail.com>
2024-09-24 18:13:08 +0530

    feat : login middleware

--- src/middlewares/user.middlewares.ts
+++ src/middlewares/user.middlewares.ts
@@ -22,3 +22,24 @@ export const registerUserMiddleware = async (
   }
   next();
 };
+
+export const loginUserMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json(
+      apiResponse.ERROR(
+        {
+          email: email ? undefined : "email is required",
+          password: password ? undefined : "password is required",
+        },
+        "All fields are required"
+      )
+    );
+  }
+  next();
+};
